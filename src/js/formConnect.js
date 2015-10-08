e33969ec8681b50af33b3d5b8c4b0a9c63d1dabb
Zach Schneider <zach@schneid.io>
2015-10-08 12:44:56 -0500

    Add mailcheck validation

--- src/js/formConnect.js
+++ src/js/formConnect.js
@@ -8,6 +8,30 @@ $('#canText').on('change', function() {
   }
 });
 
+$('#email').on('blur', function() {
+  $(this).mailcheck({
+    suggested: function(element, suggestion) {
+      $('div.email-suggestion').data('email-suggestion', suggestion.full);
+      $('div.email-suggestion .suggestion').html('Did you mean ' + suggestion.address + '@<strong>' + suggestion.domain + '</strong>?');
+      $('div.email-suggestion').show();
+    },
+    empty: function(element) {
+      $('div.email-suggestion').hide();
+    }
+  });
+});
+$('div.email-suggestion').on('click', function(event) {
+  if ($(event.target).is('.x')) {
+    return;
+  }
+  $('#email').val($(this).data('email-suggestion'));
+  $('div.email-suggestion').hide();
+});
+
+$('div.email-suggestion .x').on('click', function() {
+  $('div.email-suggestion').hide();
+});
+
 $('.signup').submit(function(e) {
   e.preventDefault();
 
